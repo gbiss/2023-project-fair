099568444708670206cd3d717cc08d477a4a7252
gbiss <gbiss@cs.umass.edu>
2023-11-24 22:33:30 +0000

    introduce LinearConstraint

--- src/valuation/constraint.py
+++ src/valuation/constraint.py
@@ -8,6 +8,18 @@ from typing import List
 def indicator(feature: BaseFeature, bundle: List[BaseItem]):
     ind = dok_array((len(feature.domain), 1), dtype=np.bool_)
     for item in bundle:
-        ind[item.index(feature, item.value(feature)), 0] = 1
+        ind[item.index(feature, item.value(feature)), 0] = True
 
     return ind
+
+
+class LinearConstraint:
+    def __init__(self, A: dok_array, b: dok_array, feature: BaseFeature):
+        self.A = A
+        self.b = b
+        self.feature = feature
+
+    def violates(self, bundle: List[BaseItem]):
+        ind = indicator(self.feature, bundle)
+
+        return np.prod((self.A @ ind <= self.b).toarray().flatten())
--- tests/test_constraint.py
+++ tests/test_constraint.py
@@ -1,5 +1,6 @@
 import numpy as np
-from valuation.constraint import indicator
+from scipy.sparse import dok_array
+from valuation.constraint import indicator, LinearConstraint
 from valuation.feature import Course
 from valuation.item import ScheduleItem
 
@@ -14,3 +15,18 @@ def test_indicator():
     ind = indicator(course, bundle)
 
     np.testing.assert_array_equal(ind.toarray().flatten(), [1, 1, 0])
+
+
+def test_linear_constraint():
+    domain = [250, 301, 611]
+    course = Course(domain)
+    bundle = [
+        ScheduleItem([course], [250]),
+        ScheduleItem([course], [301]),
+    ]
+    A = dok_array((1, len(domain)), dtype=np.int_)
+    A = A + True
+    b = dok_array((1, 1), dtype=np.bool_)
+    b = b + True
+
+    assert not LinearConstraint(A, b, course).violates(bundle)
