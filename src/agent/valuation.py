2d68255dc0769ab1c240cb2d2e669d18cad85412
gbiss <gbiss@cs.umass.edu>
2023-11-26 13:16:24 +0000

    separate valuation from memoization

--- src/agent/valuation.py
+++ src/agent/valuation.py
@@ -9,40 +9,50 @@ class BaseValuation:
     pass
 
 
-class ConstraintSatifactionValuation(BaseValuation):
-    def __init__(self, constraints: List[BaseConstraint], memoize: bool = True):
+class MemoableValuation:
+    def __init__(self, constraints: List[BaseConstraint]):
         self.constraints = constraints
-        self.memoize = memoize
-        self.marginal_memo = {}
-        self.value_memo = {}
+        self._marginal_memo = {}
+        self._value_memo = {}
+
+    def _marginal(self, bundle: List[BaseItem]):
+        raise NotImplementedError
 
     def marginal(self, bundle: List[BaseItem]):
         hashable_bundle = tuple(sorted(bundle))
 
-        if hashable_bundle in self.marginal_memo:
-            return self.marginal_memo[hashable_bundle]
+        if hashable_bundle not in self._marginal_memo:
+            self._marginal_memo[hashable_bundle] = self._marginal(bundle)
 
-        satisfies = True
-        for constraint in self.constraints:
-            satisfies *= constraint.satisfies(bundle)
-        self.marginal_memo[hashable_bundle] = satisfies
+        return self._marginal_memo[hashable_bundle]
 
-        return satisfies
+    def _value(self, bundle: List[BaseItem]):
+        raise NotImplementedError
 
     def value(self, bundle: List[BaseItem]):
         hashable_bundle = tuple(sorted(bundle))
 
-        if hashable_bundle in self.value_memo:
-            return self.value_memo[hashable_bundle]
+        if hashable_bundle not in self._value_memo:
+            self._value_memo[hashable_bundle] = self._value(bundle)
 
-        if self.marginal(bundle) == 1:
-            self.value_memo[hashable_bundle] = len(bundle)
-            return len(bundle)
+        self._value_memo[hashable_bundle]
 
+
+class ConstraintSatifactionValuation(MemoableValuation):
+    def __init__(self, constraints: List[BaseConstraint]):
+        super().__init__(constraints)
+
+    def _marginal(self, bundle: List[BaseItem]):
+        satisfies = True
+        for constraint in self.constraints:
+            satisfies *= constraint.satisfies(bundle)
+
+        return satisfies
+
+    def _value(self, bundle: List[BaseItem]):
         submarginal = 0
         for i in range(len(bundle)):
             subbundle = bundle[:i] + bundle[i + 1 :]
             submarginal = max(submarginal, self.marginal(subbundle))
-        self.value_memo[hashable_bundle] = submarginal
 
         return submarginal
