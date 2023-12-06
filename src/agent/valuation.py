7ef63016e6d914cc9e1259406100600632497417
gbiss <gbiss@cs.umass.edu>
2023-12-06 20:22:10 +0000

    add docstrings to valuation module

--- src/agent/valuation.py
+++ src/agent/valuation.py
@@ -6,23 +6,65 @@ from .constraint import BaseConstraint
 
 
 class BaseValuation:
+    """An agent's utility associated with bundles of items"""
+
     def independent(self, bundle: List[BaseItem]):
+        """Does the bundle receive maximal value
+        ]
+                Args:
+                    bundle (List[BaseItem]): Items in the bundle
+
+                Raises:
+                    NotImplemented: Must be implemented by child class
+        """
         raise NotImplemented
 
     def value(self, bundle: List[BaseItem]):
+        """Value of bundle
+
+        Args:
+            bundle (List[BaseItem]): Items in the bundle
+
+        Raises:
+            NotImplemented: Must be implemented by child class
+        """
         raise NotImplemented
 
 
 class MemoableValuation:
+    """A mixin that caches intermediate results"""
+
     def __init__(self, constraints: List[BaseConstraint]):
+        """
+        Args:
+            constraints (List[BaseConstraint]): Constraints that limit independence
+        """
         self.constraints = constraints
         self._independent_memo = {}
         self._value_memo = {}
 
     def _independent(self, bundle: List[BaseItem]):
+        """Actual calculation of bundle independence
+
+        Args:
+            bundle (List[BaseItem]): Items in the bundle
+
+        Raises:
+            NotImplementedError: Must be implemented by child class
+        """
         raise NotImplementedError
 
     def independent(self, bundle: List[BaseItem]):
+        """Does the bundle receive maximal value
+
+        Retreives cached value if present, otherwise it calculates it
+
+        Args:
+            bundle (List[BaseItem]): Items in the bundle
+
+        Returns:
+            bool: True if bundle receives maximal value; False otherwise
+        """
         hashable_bundle = tuple(sorted(bundle))
 
         if hashable_bundle not in self._independent_memo:
@@ -31,9 +73,27 @@ class MemoableValuation:
         return self._independent_memo[hashable_bundle]
 
     def _value(self, bundle: List[BaseItem]):
+        """Actual implementation of value function
+
+        Args:
+            bundle (List[BaseItem]): Items in the bundle
+
+        Raises:
+            NotImplementedError: Must be implemented by child class
+        """
         raise NotImplementedError
 
     def value(self, bundle: List[BaseItem]):
+        """Value of bundle
+
+        Retreives cached value if present, otherwise it calculates it
+
+        Args:
+            bundle (List[BaseItem]): Items in the bundle
+
+        Returns:
+            int: Bundle value
+        """
         hashable_bundle = tuple(sorted(bundle))
 
         if hashable_bundle not in self._value_memo:
@@ -43,10 +103,24 @@ class MemoableValuation:
 
 
 class ConstraintSatifactionValuation(MemoableValuation):
+    """Valuation that limits independence with constraints"""
+
     def __init__(self, constraints: List[BaseConstraint]):
+        """
+        Args:
+            constraints (List[BaseConstraint]): Constraints that limit independence
+        """
         super().__init__(constraints)
 
     def _independent(self, bundle: List[BaseItem]):
+        """Does the bundle receive maximal value
+
+        Args:
+            bundle (List[BaseItem]): Items in the bundle
+
+        Returns:
+            bool: True if bundle receives maximal value; False otherwise
+        """
         satisfies = True
         for constraint in self.constraints:
             satisfies *= constraint.satisfies(bundle)
@@ -54,6 +128,16 @@ class ConstraintSatifactionValuation(MemoableValuation):
         return satisfies
 
     def _value(self, bundle: List[BaseItem]):
+        """Value of bundle
+
+        The value is the size of the largest independent set contained in the bundle
+
+        Args:
+            bundle (List[BaseItem]): Items in the bundle
+
+        Returns:
+            int: Bundle value
+        """
         if self.independent(bundle):
             return len(bundle)
 
@@ -66,5 +150,11 @@ class ConstraintSatifactionValuation(MemoableValuation):
 
 
 class StudentValuation(ConstraintSatifactionValuation):
+    """A constraint-based student valuation"""
+
     def __init__(self, constraints: List[BaseConstraint]):
+        """
+        Args:
+            constraints (List[BaseConstraint]): Constraints that limit independence
+        """
         super().__init__(constraints)
