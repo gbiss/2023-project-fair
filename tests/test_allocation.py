dbd6e1e5bcf8a6311ace3a5fda89c2709ea4732d
gbiss <gbiss@cs.umass.edu>
2023-12-13 23:09:00 +0000

    extract renaissance examples into fixtures

--- tests/test_allocation.py
+++ tests/test_allocation.py
@@ -6,30 +6,12 @@ from fair.item import ScheduleItem
 from fair.simulation import RenaissanceMan
 
 
-def test_legacy_simulated_student(
-    course: Course, slot: Slot, section: Section, schedule: list[ScheduleItem]
+def test_original_yankee_swap(
+    renaissance1: RenaissanceMan,
+    renaissance2: RenaissanceMan,
+    schedule: list[ScheduleItem],
 ):
-    course_time_constr = CourseTimeConstraint.mutually_exclusive_slots(
-        schedule, course, slot
-    )
-    course_sect_constr = CourseSectionConstraint.one_section_per_course(
-        schedule, course, section
-    )
-    student1 = RenaissanceMan(
-        [["250", "301"], ["611"]],
-        [1, 1],
-        course,
-        [course_time_constr, course_sect_constr],
-        0,
-    )
-    student2 = RenaissanceMan(
-        [["250", "301"], ["611"]],
-        [1, 1],
-        course,
-        [course_time_constr, course_sect_constr],
-        1,
-    )
-    leg_student1 = LegacyStudent(student1)
-    leg_student2 = LegacyStudent(student2)
+    leg_student1 = LegacyStudent(renaissance1)
+    leg_student2 = LegacyStudent(renaissance2)
 
     original_yankee_swap([leg_student1, leg_student2], schedule)
