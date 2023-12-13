dcffe67d35874a045dfe89ac98dd465cc22baaff
gbiss <gbiss@cs.umass.edu>
2023-12-13 23:31:37 +0000

    add general YS test

--- tests/test_allocation.py
+++ tests/test_allocation.py
@@ -1,7 +1,5 @@
 from fair.agent import LegacyStudent
-from fair.allocation import original_yankee_swap
-from fair.constraint import CourseSectionConstraint, CourseTimeConstraint
-from fair.feature import Course, Section, Slot
+from fair.allocation import general_yankee_swap, original_yankee_swap
 from fair.item import ScheduleItem
 from fair.simulation import RenaissanceMan
 
@@ -15,3 +13,14 @@ def test_original_yankee_swap(
     leg_student2 = LegacyStudent(renaissance2)
 
     original_yankee_swap([leg_student1, leg_student2], schedule)
+
+
+def test_general_yankee_swap(
+    renaissance1: RenaissanceMan,
+    renaissance2: RenaissanceMan,
+    schedule: list[ScheduleItem],
+):
+    leg_student1 = LegacyStudent(renaissance1)
+    leg_student2 = LegacyStudent(renaissance2)
+
+    general_yankee_swap([leg_student1, leg_student2], schedule)
