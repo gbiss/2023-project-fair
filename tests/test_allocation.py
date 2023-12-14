d557717023b218c4ccaad125fd8d0e67c1678804
gbiss <gbiss@cs.umass.edu>
2023-12-14 22:13:12 +0000

    implement get_desired_items_indexes

--- tests/test_allocation.py
+++ tests/test_allocation.py
@@ -1,5 +1,11 @@
 from fair.agent import LegacyStudent
-from fair.allocation import bfs_yankee_swap, general_yankee_swap, original_yankee_swap
+from fair.allocation import (
+    SPIRE_algorithm,
+    bfs_yankee_swap,
+    general_yankee_swap,
+    original_yankee_swap,
+    round_robin,
+)
 from fair.item import ScheduleItem
 from fair.simulation import RenaissanceMan
 
@@ -35,3 +41,25 @@ def test_bfs_yankee_swap(
     leg_student2 = LegacyStudent(renaissance2)
 
     bfs_yankee_swap([leg_student1, leg_student2], schedule)
+
+
+def test_round_robin_swap(
+    renaissance1: RenaissanceMan,
+    renaissance2: RenaissanceMan,
+    schedule: list[ScheduleItem],
+):
+    leg_student1 = LegacyStudent(renaissance1)
+    leg_student2 = LegacyStudent(renaissance2)
+
+    round_robin([leg_student1, leg_student2], schedule)
+
+
+def test_spire_swap(
+    renaissance1: RenaissanceMan,
+    renaissance2: RenaissanceMan,
+    schedule: list[ScheduleItem],
+):
+    leg_student1 = LegacyStudent(renaissance1)
+    leg_student2 = LegacyStudent(renaissance2)
+
+    SPIRE_algorithm([leg_student1, leg_student2], schedule)
