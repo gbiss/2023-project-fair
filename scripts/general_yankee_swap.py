8b2a18aff5325f04d89700d90afeb722965d9895
George Bissias <gbiss@cs.umass.edu>
2023-12-22 20:50:36 -0500

    reformat general_yankee_swap.py

--- scripts/general_yankee_swap.py
+++ scripts/general_yankee_swap.py
@@ -8,9 +8,8 @@ from fair.allocation import general_yankee_swap
 from fair.constraint import CourseTimeConstraint, MutualExclusivityConstraint
 from fair.feature import Course, Section, Slot, slots_for_time_range
 from fair.item import ScheduleItem
+from fair.metrics import leximin, nash_welfare, utilitarian_welfare
 from fair.simulation import RenaissanceMan
-
-from fair.metrics import utilitarian_welfare, nash_welfare, leximin
 
 NUM_STUDENTS = 3
 MAX_COURSES_PER_TOPIC = 5
@@ -67,9 +66,9 @@ for i in range(NUM_STUDENTS):
     )
     students.append(LegacyStudent(student, student.all_courses_constraint))
 
-X=general_yankee_swap(students, schedule)
-print('utilitarian welfare: ', utilitarian_welfare(X[0],students,schedule))
-print('nash welfare: ', nash_welfare(X[0],students,schedule))
-print('leximin vector: ', leximin(X[0],students,schedule))
+X = general_yankee_swap(students, schedule)
+print("utilitarian welfare: ", utilitarian_welfare(X[0], students, schedule))
+print("nash welfare: ", nash_welfare(X[0], students, schedule))
+print("leximin vector: ", leximin(X[0], students, schedule))
 print("total bundles evaluated", [student.valuation.valuation._value_ct])
 print("unique bundles evaluated", [student.valuation.valuation._unique_value_ct])
