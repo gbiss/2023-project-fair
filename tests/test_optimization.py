1311f16c2a26b1a7a82348ef744e7d07cbff8d0e
George Bissias <gbiss@cs.umass.edu>
2024-01-10 16:53:07 -0500

    add capacity constraints to ILP

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-from fair.agent import LegacyStudent
+from fair.agent import BaseAgent, LegacyStudent
 from fair.allocation import general_yankee_swap
+from fair.constraint import PreferenceConstraint
+from fair.feature import Course
 from fair.item import ScheduleItem
 from fair.metrics import utilitarian_welfare
-from fair.optimization import IntegerLinearProgram
+from fair.optimization import IntegerLinearProgram, StudentAllocationProgram
 from fair.simulation import RenaissanceMan
+from fair.valuation import ConstraintSatifactionValuation
 
 
 def test_integer_linear(
@@ -37,3 +40,35 @@ def test_integer_linear(
 
     # YS solution should not exceed ilp solution
     assert sum(opt_alloc) / len(agents) >= util
+
+
+def test_capacity_constraint(course: Course):
+    # course capacities are 1 by default
+    schedule = [
+        ScheduleItem([course], ["250"], 0),
+        ScheduleItem([course], ["301"], 1),
+        ScheduleItem([course], ["611"], 2),
+    ]
+    constraint = PreferenceConstraint.from_item_lists(
+        schedule, [["250", "301", "611"]], [3], course
+    )
+    valuation = ConstraintSatifactionValuation([constraint])
+
+    # two students with exactly the same preferences
+    agents = [BaseAgent(valuation), BaseAgent(valuation)]
+
+    program = StudentAllocationProgram(agents, schedule).compile()
+    opt_alloc = program.formulateUSW().solve()
+
+    # only three courses total could have been allocated
+    assert np.sum(opt_alloc) == 3
+
+    # change course capacities to 2
+    for item in schedule:
+        item.capacity = 2
+
+    program = StudentAllocationProgram(agents, schedule).compile()
+    opt_alloc = program.formulateUSW().solve()
+
+    # now it's possible to allocate each of the three courses twice
+    assert np.sum(opt_alloc) == 6
