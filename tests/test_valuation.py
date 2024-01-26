24f2187e58e8c2dd3500bfaf559461cea69b0c48
David <dpthibodeau@umass.edu>
2024-01-26 13:21:58 -0500

    added functions and tests to check for submodularity, monotonicity, MRF, etc.

--- tests/test_valuation.py
+++ tests/test_valuation.py
@@ -1,9 +1,19 @@
+# from tests.conftest import length_marginal_decreasing, length_marginal_increasing
+import math
 from typing import List
 
 from fair.constraint import LinearConstraint, PreferenceConstraint
 from fair.feature import Course, Section, Slot, Weekday
 from fair.item import ScheduleItem
-from fair.valuation import ConstraintSatifactionValuation, UniqueItemsValuation, is_mrf
+from fair.valuation import (
+    ConstraintSatifactionValuation,
+    UniqueItemsValuation,
+    is_monotonic_non_decreasing,
+    is_mrf,
+    is_submodular,
+    nonnegative_rank_value,
+    rank_value_leq_cardinality,
+)
 
 
 def test_valid_constraint_valuation(
@@ -91,4 +101,82 @@ def test_mrf(course: Course, slot: Slot, weekday: Weekday, section: Section):
     valuation = ConstraintSatifactionValuation([constraint])
     assert not is_mrf(schedule, valuation.value)
 
-    # add example where is it MRF
+    # is MRF
+    values = [0, 1, 2, 3, 4, 5]
+    assert is_mrf(values, length_marginal_decreasing)
+
+
+
+def test_submodular():
+    values = [0, 1, 2, 3, 4, 5]
+
+    assert not is_submodular(values, length_marginal_increasing)
+    assert is_submodular(values, length_marginal_decreasing)
+    assert is_submodular(values, additive)
+    assert is_submodular(values, budget_additive)
+    assert not is_submodular(values, additive_divide_by_rank)
+
+
+def test_monotonic_non_decreasing():
+    vals_1 = [0, 1, 2, 3, 4, 5]
+
+    assert is_monotonic_non_decreasing(vals_1, length_marginal_increasing)
+    assert is_monotonic_non_decreasing(vals_1, length_marginal_decreasing)
+    assert is_monotonic_non_decreasing(vals_1, additive)
+    assert is_monotonic_non_decreasing(vals_1, budget_additive)
+    assert not is_monotonic_non_decreasing(vals_1, additive_divide_by_rank)
+
+
+def test_nonnegative_rank_value():
+    values = [0, 1, 2, 3, 4, 5]
+
+    assert nonnegative_rank_value(values, length_marginal_increasing)
+    assert nonnegative_rank_value(values, length_marginal_decreasing)
+    assert nonnegative_rank_value(values, additive)
+    assert nonnegative_rank_value(values, budget_additive)
+    assert nonnegative_rank_value(values, additive_divide_by_rank)
+
+
+def test_rank_value_leq_cardinality():
+    values = [0, 1, 2, 3, 4, 5]
+
+    assert not rank_value_leq_cardinality(values, length_marginal_increasing)
+    assert rank_value_leq_cardinality(values, length_marginal_decreasing)
+    assert not rank_value_leq_cardinality(values, additive)
+    assert not rank_value_leq_cardinality(values, budget_additive)
+    assert not rank_value_leq_cardinality(values, additive_divide_by_rank)
+
+
+def length_marginal_increasing(vals):
+    """
+    FAIL submodular, PASS monotone non-decreasing
+    """
+    return len(vals) ** 2
+
+
+def length_marginal_decreasing(vals):
+    """
+    PASS submodular, PASS monotone non-decreasing
+    """
+    return math.sqrt(len(vals))
+
+
+def additive(vals):
+    """
+    PASS submodular, PASS monotone non-decreasing
+    """
+    return sum(vals)
+
+
+def budget_additive(vals):
+    """
+    PASS submodular, PASS monotone non-decreasing
+    """
+    return min(sum(vals), 3)
+
+
+def additive_divide_by_rank(vals):
+    """
+    FAIL submodular, FAIL monotone non-decreasing
+    """
+    return sum(vals) / (len(vals) + 1)
