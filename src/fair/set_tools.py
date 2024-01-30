f2c69510339c067450a9e5f492ff71146b84bdbf
David <dpthibodeau@umass.edu>
2024-01-30 15:37:31 -0500

    Fixed type hint issue

--- src/fair/set_tools.py
+++ src/fair/set_tools.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable
 from itertools import chain, combinations
 
 
@@ -14,12 +15,12 @@ def powerset(iterable: list):
     return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
 
-def is_mrf(ground: list, func: function):
+def is_mrf(ground: list, func: Callable):
     """Checks 4 conditions of valid rank functions
 
     Args:
         ground (list[Any]): Ground set of items (bundle)
-        func (typing.Callable): Function to be tested
+        func (collections.abc.Callable): Function to be tested
 
     Returns:
         bool: True if func is an MRF, given ground; False otherwise
@@ -32,12 +33,12 @@ def is_mrf(ground: list, func: function):
     return check1 and check2 and check3 and check4
 
 
-def is_submodular(ground: list, func: function):
+def is_submodular(ground: list, func: Callable):
     """Performs check for submodularity of func, given a ground set of items
 
     Args:
         ground (list[Any]): Gound set of items (bundle)
-        func (typing.Callable): Function to be tested
+        func (collections.abc.Callable): Function to be tested
 
     Returns:
         bool: True if func is submodular, given ground; False otherwise
@@ -59,12 +60,12 @@ def is_submodular(ground: list, func: function):
     return True
 
 
-def is_monotonic_non_decreasing(ground: list, func: function):
+def is_monotonic_non_decreasing(ground: list, func: Callable):
     """Checks that func is monotonic non-decreasing, given the ground set, ground
 
     Args:
         ground (list[Any]): Ground set of items (bundle)
-        func (typing.Callable): Function to be tested
+        func (collections.abc.Callable): Function to be tested
 
     Returns:
         bool: True if func is monotonic non_decreasing, given ground; False otherwise
@@ -76,18 +77,20 @@ def is_monotonic_non_decreasing(ground: list, func: function):
             set1_subset_set2 = False
             if set(powerset_ground[i]).issubset(set(powerset_ground[j])):
                 set1_subset_set2 = True
-            if set1_subset_set2 == True and func(powerset_ground[i]) > func(powerset_ground[j]):
+            if set1_subset_set2 == True and func(powerset_ground[i]) > func(
+                powerset_ground[j]
+            ):
                 return False
 
     return True
 
 
-def nonnegative_rank_value(ground: list, func: function):
+def nonnegative_rank_value(ground: list, func: Callable):
     """Checks that func always returns a non-negative value, given the ground set, ground
 
     Args:
         ground (list[Any]): Ground set of items (bundle)
-        func (typing.Callable): Function to be tested
+        func (collections.abc.Callable): Function to be tested
     Returns:
         bool: True if func returns non-negative value, given ground; False otherwise
     """
@@ -100,12 +103,12 @@ def nonnegative_rank_value(ground: list, func: function):
     return True
 
 
-def rank_value_leq_cardinality(ground: list, func: function):
+def rank_value_leq_cardinality(ground: list, func: Callable):
     """Checks that func always returns a value less than the cardinality
 
     Args:
         ground (list[Any]): Ground set of items (bundle)
-        func (typing.Callable): Function to be tested
+        func (collections.abc.Callable): Function to be tested
 
     Returns:
         bool: True if func returns a value less than the cardinality, given ground; False otherwise
