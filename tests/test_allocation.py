fd0c46148eb5db76f63d3caaf135d4c8fb84d009
Paula Navarrete Díaz <paula@Paulas-MacBook-Air.local>
2024-02-18 18:24:48 -0500

    delete tests associated to obsolete allocation methods

--- tests/test_allocation.py
+++ tests/test_allocation.py
@@ -1,38 +1,13 @@
 from fair.agent import LegacyStudent
 from fair.allocation import (
     SPIRE_algorithm,
-    bfs_yankee_swap,
     general_yankee_swap,
-    original_yankee_swap,
+    general_yankee_swap_E,
     round_robin,
 )
 from fair.feature import Course
 from fair.item import ScheduleItem
 from fair.simulation import RenaissanceMan
-
-
-def test_original_yankee_swap(
-    renaissance1: RenaissanceMan,
-    renaissance2: RenaissanceMan,
-    schedule: list[ScheduleItem],
-    course: Course,
-):
-    leg_student1 = LegacyStudent(renaissance1, renaissance1.preferred_courses, course)
-    leg_student2 = LegacyStudent(renaissance2, renaissance2.preferred_courses, course)
-
-    X, _, _ = original_yankee_swap([leg_student1, leg_student2], schedule)
-    alloc1 = X[:, 0]
-    alloc2 = X[:, 1]
-
-    # quantity allocated courses does not exceed limit
-    assert sum(alloc1) <= renaissance1.total_courses
-    assert sum(alloc2) <= renaissance2.total_courses
-
-    # only preferred courses are allocated
-    courses1 = [schedule[i].value(course) for i in range(len(alloc1)) if alloc1[i] == 1]
-    courses2 = [schedule[i].value(course) for i in range(len(alloc2)) if alloc2[i] == 1]
-    assert set(courses1) <= set(renaissance1.preferred_courses)
-    assert set(courses2) <= set(renaissance2.preferred_courses)
 
 
 def test_general_yankee_swap(
@@ -59,7 +34,7 @@ def test_general_yankee_swap(
     assert set(courses2) <= set(renaissance2.preferred_courses)
 
 
-def test_bfs_yankee_swap(
+def test_general_yankee_swap_E(
     renaissance1: RenaissanceMan,
     renaissance2: RenaissanceMan,
     schedule: list[ScheduleItem],
@@ -68,7 +43,7 @@ def test_bfs_yankee_swap(
     leg_student1 = LegacyStudent(renaissance1, renaissance1.preferred_courses, course)
     leg_student2 = LegacyStudent(renaissance2, renaissance2.preferred_courses, course)
 
-    X = bfs_yankee_swap([leg_student1, leg_student2], schedule)
+    X, _, _ = general_yankee_swap_E([leg_student1, leg_student2], schedule)
     alloc1 = X[:, 0]
     alloc2 = X[:, 1]
 
